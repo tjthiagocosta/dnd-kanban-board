495f9a544d3648a938c4ea1f196527c1a6122db8
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-10-29 22:13:11 -0300

    fix: add property colors to extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      mainBackgroundColor: "#0D1117",
-      ColumnBackgroundColor: "#161C22",
+      colors: {
+        mainBackgroundColor: "#0D1117",
+        columnBackgroundColor: "#161C22",
+      },
     },
   },
   plugins: [],
